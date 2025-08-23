547226566d88bebbd853a99898e1ed6e96d3be40
MohamedMagdy63 <mohammedmagdy6522@gmail.com>
2025-08-23 04:38:08 +0300

    Deploy error fix

--- src/components/foodCard/FoodCard.tsx
+++ src/components/foodCard/FoodCard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import {
